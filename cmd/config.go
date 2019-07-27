a784db156f6b7d8c59ac9303e6d5901a3e14a38f
CJ Harries <cj@wizardsoftheweb.pro>
2019-07-27 18:44:28 -0500

    Create helper to build session

--- cmd/config.go
+++ cmd/config.go
@@ -1,6 +1,30 @@
 package wotwhb
 
+import (
+	"net/http"
+	"path/filepath"
+
+	cookiejar "github.com/juju/persistent-cookiejar"
+	"golang.org/x/net/publicsuffix"
+)
+
 func BootstrapConfig(configPath, downloadPath string) {
 	ensureDirectoryExists(configPath)
 	ensureDirectoryExists(downloadPath)
+}
+
+func buildSession() (*http.Client, *cookiejar.Jar) {
+	options := cookiejar.Options{
+		PublicSuffixList: publicsuffix.List,
+		Filename:         filepath.Join(ConfigDirectoryFlagValue, cookieFileBasename),
+	}
+	jar, err := cookiejar.New(&options)
+	if nil != err {
+		Logger.Fatal(err)
+	}
+	Logger.Trace(jar.AllCookies())
+	client := &http.Client{
+		Jar: jar,
+	}
+	return client, jar
 }
