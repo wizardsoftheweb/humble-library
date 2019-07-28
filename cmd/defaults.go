c6715de6cc6ec7824545779fbb25e95f220b341a
CJ Harries <cj@wizardsoftheweb.pro>
2019-07-27 19:28:03 -0500

    Add more strings

--- cmd/defaults.go
+++ cmd/defaults.go
@@ -14,3 +14,14 @@ var (
 	configDirectory   = filepath.Clean(filepath.Join("~", ".config", "wotw", "humblebundle"))
 	downloadDirectory = filepath.Clean(filepath.Join("~", "Downloads"))
 )
+
+const (
+	usernameLongPrompt   = "Please enter your Humble Username/Email."
+	usernameShortPrompt  = "Username"
+	passwordLongPrompt   = "Please enter your Humble Password."
+	passwordShortPrompt  = "Password"
+	recaptchaLongPrompt  = "Please visit https://www.schiff.io/projects/humble-bundle-api#getting-a-captcha to learn how to get a Recaptcha code. Once you've got one, enter it here."
+	recaptchaShortPrompt = "Recaptcha Code"
+	guardLongPrompt      = "Please check your email for a Humble Bundle account protection code."
+	guardShortPrompt     = "Code"
+)
