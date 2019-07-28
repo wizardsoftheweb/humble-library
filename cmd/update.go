233551aed302670dd08cf42c65da42cccd7e6b8d
CJ Harries <cj@wizardsoftheweb.pro>
2019-07-27 22:32:14 -0500

    Attach the update command

--- cmd/update.go
+++ cmd/update.go
@@ -4,9 +4,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func init() {
+	PackageCmd.AddCommand(UpdateCmd)
+}
+
 var UpdateCmd = &cobra.Command{
 	Use:   "update",
 	Short: "Update local databases",
+	Run:   UpdateCmdRun,
 }
 
 func UpdateCmdRun(cmd *cobra.Command, args []string) {
