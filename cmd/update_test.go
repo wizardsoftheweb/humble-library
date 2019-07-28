0241ae8b4974da61880d266ff871d65f0df73268
CJ Harries <cj@wizardsoftheweb.pro>
2019-07-27 23:31:46 -0500

    Test update commands

---
+++ cmd/update_test.go
@@ -0,0 +1,47 @@
+package wotwhb
+
+import (
+	"io/ioutil"
+	"strings"
+
+	"github.com/spf13/cobra"
+	. "gopkg.in/check.v1"
+)
+
+type UpdateSuite struct {
+	BaseSuite
+	Command *cobra.Command
+	Args    []string
+}
+
+func (s *UpdateSuite) Printf(format string, args ...interface{}) {}
+
+var _ = Suite(&UpdateSuite{})
+
+func (s *UpdateSuite) SetUpTest(c *C) {
+	s.Command = &cobra.Command{}
+	s.Command.SetOut(ioutil.Discard)
+	s.Command.SetErr(ioutil.Discard)
+	s.Args = []string{}
+	inputReader = strings.NewReader(inputTestResult + "\n")
+}
+
+func (s *UpdateSuite) TestUpdateOrderListCmdRun(c *C) {
+	c.Assert(
+		func() {
+			UpdateOrderListCmdRun(s.Command, s.Args)
+		},
+		PanicMatches,
+		".*EOF.*",
+	)
+}
+
+func (s *UpdateSuite) TestUpdateCmdRun(c *C) {
+	c.Assert(
+		func() {
+			UpdateCmdRun(s.Command, s.Args)
+		},
+		Not(PanicMatches),
+		".*",
+	)
+}
